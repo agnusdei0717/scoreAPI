2f6f65f3bfb5ba248acfd73f42b15f93c2e6cf30
Gimin <agnusdei0717@gmail.com>
2021-02-03 14:39:27 +0800

    Initial commit

---
+++ src/main/java/com/bofa/payment/scoreAPI/ScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.bofa.payment.scoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bofa/payment/scoreAPI/ScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bofa.payment.scoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
